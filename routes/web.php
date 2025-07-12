d1b513ef53f85691660b0de6bf955183777322c2
Beyza UÇAK <144684543+ucakbeyza@users.noreply.github.com>
2025-07-12 11:29:29 +0300

    Routing and Creating New Page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+//This structure is generally preferred if dynamic operations are to be performed.
+/* 
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/second', function(){
+    return view('second');
+});
+*/
+//If only a view is to be returned, you do not need to define a function.
+Route::view('/','welcome'); 
+Route::view('/second','second');
